e34cea47cc5ca854d404f8a8f97ae0a6ff4dd67f
lunatic0der <83530725+lunatic0der@users.noreply.github.com>
2021-12-17 17:30:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ from config import (
     FORCE_SUB_CHANNEL,
     FORCE_SUB_CHANNEL2,
     FORCE_SUB_CHANNEL3,
-    FORCE_SUB_CHANNEL4,
     LOGGER,
     OWNER,
     TG_BOT_TOKEN,
@@ -80,22 +79,6 @@ class Bot(Client):
                 )
                 self.LOGGER(__name__).warning(
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL3 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
-                )
-                self.LOGGER(__name__).info(
-                    "Bot Berhenti. Tanyakan Ke https://t.me/mrismanaziz untuk Bantuan"
-                )
-                sys.exit()
-        if FORCE_SUB_CHANNEL4:
-            try:
-                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL4)
-                self.invitelink4 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning(
-                    "Bot tidak dapat Mengambil link Undangan dari Force Sub Channel!"
-                )
-                self.LOGGER(__name__).warning(
-                    f"Silakan periksa kembali var FORCE_SUB_CHANNEL4 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
                     "Bot Berhenti. Tanyakan Ke https://t.me/mrismanaziz untuk Bantuan"
